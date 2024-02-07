e62da3a1d5e1b1ba03ccc6bddbdec1914392d9c7
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-02-08 06:17:04 +0900

    ローディングの解除を行い忘れていた為、追加

--- src/app/(private)/template.tsx
+++ src/app/(private)/template.tsx
@@ -25,6 +25,7 @@ export default function Template({children}: {children: React.ReactNode}) {
                 return;
             }
         }
+        setIsLoading(false);
     }, [isSignedIn, isLoaded, session, pathname, router]);
 
     if (!(isSignedIn || isLoaded)) return <></>;
