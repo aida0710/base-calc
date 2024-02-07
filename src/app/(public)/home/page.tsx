639c1b6085c464a21b64f35b9ae8b5fd8a161852
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-02-08 05:59:24 +0900

    ホームを形だけ追加

---
+++ src/app/(public)/home/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default async function Page() {
+    return (
+        <div>
+            <p className='my-5 text-center text-default-500'>
+                <Link href='https://aida0710.work'>Aida0710が運営中</Link>
+            </p>
+        </div>
+    );
+}
